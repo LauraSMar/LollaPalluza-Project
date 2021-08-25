eaa5e29595a5635ff6d568d64688a6b465982419
franriverom <frannrivero@gmail.com>
2021-08-25 11:16:58 -0300

    metodos comentarios

--- Lollapalloza/src/main/resources/static/js/gallery.js
+++ Lollapalloza/src/main/resources/static/js/gallery.js
@@ -3,8 +3,15 @@ const app = Vue.createApp({
         return {
             email: "",
             password: "",
-            notLogged: false,
+            commentId: "",
+            imageId: "",
+            commentText: "",
+            error: false,
+            comments: [],
         }
+    },
+    created(){
+        this.allComments()
     },
     methods: {
         login() {
@@ -14,7 +21,42 @@ const app = Vue.createApp({
                         window.location.href = "/admin.html"
                     } else window.location.href = "/index.html"
                 })
-                .catch(() => swal('Wrong mail or password'))
+                .catch(() => swal('Email o contraseña incorrectos'))
         },
-    },
-})
+        postComment(){
+            axios.post('/api/images'+this.imageId+'/comments',{
+                text: this.commentText})
+                .then(()=> swal("Comentario enviado exitosamente", {
+                    icon: "success",
+                  })
+                .then(() => location.reload))
+                .catch(() => {swal('Necesitas estar logueado para poder comentar');
+                 this.error = !this.error});
+        },
+        allComments(){
+            axios.get('/api/images/1/comments')
+            .then(res=> 
+                this.comments = res.data)
+        },
+
+        fetch(){
+            axios.get('/api/comments'+this.commentId)
+        },
+
+        editComment(){
+            axios.post('/api/comments'+this.commentId,{
+                newText: this.commentText})
+                .then(()=> swal("Comentario editado exitosamente", {
+                    icon: "success",
+                  })
+                .then(() => location.reload))
+        },
+        deleteComment(){
+            axios.delete('/api/comments'+this.commentId)
+                .then(()=> swal("success deleting your comment", {
+                    icon: "success",
+                  })
+                .then(() => location.reload))
+        },
+    }
+});
