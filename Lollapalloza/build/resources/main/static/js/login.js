f6580258aca9babfd6b2683038c5e1d3c5edd831
paaherre <68233239+paaherre@users.noreply.github.com>
2021-08-26 15:59:02 -0300

    updates Cart

--- Lollapalloza/build/resources/main/static/js/login.js
+++ Lollapalloza/build/resources/main/static/js/login.js
@@ -9,19 +9,16 @@ const app = Vue.createApp({
             postPassword: "",
             documentId:"",
             isUser: true,
-            options: false,
         }
     },
     methods: {
         login() {
             axios.post('/api/login', "email=" + this.email + "&password=" + this.password, { headers: { 'content-type': 'application/x-www-form-urlencoded' } })
-                .then(()=> this.options = !this.options)
                 .then(() => {
                     if (this.email == "admin@admin.com") {
                         window.location.href = "/admin.html"
                     } else window.location.href = "/index.html"
                 })
-                
                 .catch(() => swal('Wrong mail or password'))
         },
         register() {
--- Lollapalloza/build/resources/main/static/js/productDetails.js
+++ Lollapalloza/build/resources/main/static/js/productDetails.js
@@ -1,46 +1,87 @@
 const app = Vue.createApp({
-    data() {
-      return {
-        /* Navbar */ 
-        activeNavbar: false,
-        activeMenu: false,
-        
-        /* Products */
-        selectedProduct: "",
-        activeImg: 0,
-        selectedSize: "",
-        quantity: 0,
-      };
-    },
-    created(){
-        axios.get("/api/products")
-        .then(res => {
-          let params = new URLSearchParams(document.location.search.substring(1));
-          let productId = params.get("productId")
-          console.log(productId)
-          this.selectedProduct = res.data.filter(p => p.id == productId)[0]
-          console.log(this.selectedProduct)
-        })
-    },
-    methods: {
-      selectImg(i){
-        this.activeImg = i;
+  data() {
+    return {
+      /* Navbar */
+      activeNavbar: false,
+      activeMenu: false,
+
+      /* Products */
+      selectedProduct: "",
+      activeImg: 0,
+      selectedSize: "",
+      quantity: 1,
+
+      /* Cart */
+      cart: {
+        cartdtos: [],
+        ticketDtos: [],
       },
-      selectSize(i){
-        this.selectedSize = i;
-      },
-      formatBalance(balance) {
-        if (balance == null) {
-            return
-        }
-        let amount = new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD',
-        })
-        return amount.format(balance)
-      },
+    };
+  },
+  created() {
+    axios.get("/api/products")
+      .then(res => {
+        let params = new URLSearchParams(document.location.search.substring(1));
+        let productId = params.get("productId")
+        console.log(productId)
+        this.selectedProduct = res.data.filter(p => p.id == productId)[0]
+        console.log(this.selectedProduct)
+      })
+    if (JSON.parse(sessionStorage.getItem("cart"))) {
+      this.cart = JSON.parse(sessionStorage.getItem("cart"))
+    } else {
+      sessionStorage.setItem("cart", JSON.stringify(this.cart))
+    }
+
+  },
+  methods: {
+    selectImg(i) {
+      this.activeImg = i;
     },
-  
-    computed: {},
-  });
-  
+    selectSize(i) {
+      this.selectedSize = i;
+    },
+    formatBalance(balance) {
+      if (balance == null) {
+        return
+      }
+      let amount = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      })
+      return amount.format(balance)
+    },
+    addToCart() {
+      let productToCart = {
+        "idItem": this.selectedProduct.id,
+        "category": this.selectedProduct.category,
+        "quantity": this.quantity
+      }
+
+      if (this.cart.cartdtos.length == 0){
+        this.cart.cartdtos.push(productToCart)
+        sessionStorage.setItem("cart", JSON.stringify(this.cart))
+        return
+      }
+      
+      let arrayAux = this.cart.cartdtos.map(e => e.idItem)
+
+      if (arrayAux.indexOf(productToCart.idItem) == -1){
+        console.log(arrayAux)
+        this.cart.cartdtos.push(productToCart)
+      }
+
+      if (arrayAux.indexOf(productToCart.idItem) > -1) {
+          this.cart.cartdtos.map((e) => {
+            if (e.idItem == productToCart.idItem) {
+              e.quantity += productToCart.quantity
+            }
+          })
+      } 
+      sessionStorage.setItem("cart", JSON.stringify(this.cart))
+      console.log(this.cart.cartdtos)
+    }
+  },
+
+  computed: {},
+});
