f8136850ce72f9a650df5fe2cc0db7c18e9123c4
Kevin17ar <81791483+Kevin17ar@users.noreply.github.com>
2021-08-27 03:51:50 -0300

    form payment

--- Lollapalloza/src/main/java/LollapallozaProject/Lollapalloza/dtos/EventDto.java
+++ Lollapalloza/src/main/java/LollapallozaProject/Lollapalloza/dtos/EventDto.java
@@ -3,16 +3,19 @@ package LollapallozaProject.Lollapalloza.dtos;
 import LollapallozaProject.Lollapalloza.models.Event;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.JoinColumn;
+import java.text.DateFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 @Data
 @NoArgsConstructor
 public class EventDto {
         private Long id;
-        private LocalDate date;
+        private String date;
         private LocalTime start;
         private LocalTime end;
         private String img;
@@ -22,12 +25,17 @@ public class EventDto {
 
         public EventDto(Event event) {
             this.id = event.getId();
-            this.date = event.getDate();
+            this.date = formatterDate(event.getDate());
             this.start= event.getStart();
             this.end= event.getEnd();
             this.campus= event.getCampus().getLocation();
             this.img = event.getImg();
             this.available = event.getCampus().getCapacity();
+        }
+
+        public String formatterDate(LocalDate date){
+            DateTimeFormatter myFormatter = DateTimeFormatter.ofPattern("dd MMM");
+            return date.format(myFormatter);
         }
 
 }
