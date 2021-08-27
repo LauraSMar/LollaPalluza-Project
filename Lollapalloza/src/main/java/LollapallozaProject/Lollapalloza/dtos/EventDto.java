009fa78ca436898008056ad891c3e5e97382e725
franriverom <frannrivero@gmail.com>
2021-08-27 11:03:04 -0300

    Revert "Merge branch 'main' of https://github.com/LauraSMar/LollaPalluza-Project into main"
    This reverts commit df9073b90926699de1b97e9b1531026b5fabafe9, reversing
    changes made to f83a6631f4e26f6cafb7e1b66a1355e6767c3671.

--- Lollapalloza/src/main/java/LollapallozaProject/Lollapalloza/dtos/EventDto.java
+++ Lollapalloza/src/main/java/LollapallozaProject/Lollapalloza/dtos/EventDto.java
@@ -3,19 +3,16 @@ package LollapallozaProject.Lollapalloza.dtos;
 import LollapallozaProject.Lollapalloza.models.Event;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.JoinColumn;
-import java.text.DateFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 
 @Data
 @NoArgsConstructor
 public class EventDto {
         private Long id;
-        private String date;
+        private LocalDate date;
         private LocalTime start;
         private LocalTime end;
         private String img;
@@ -25,17 +22,12 @@ public class EventDto {
 
         public EventDto(Event event) {
             this.id = event.getId();
-            this.date = formatterDate(event.getDate());
+            this.date = event.getDate();
             this.start= event.getStart();
             this.end= event.getEnd();
             this.campus= event.getCampus().getLocation();
             this.img = event.getImg();
             this.available = event.getCampus().getCapacity();
-        }
-
-        public String formatterDate(LocalDate date){
-            DateTimeFormatter myFormatter = DateTimeFormatter.ofPattern("dd MMM");
-            return date.format(myFormatter);
         }
 
 }
